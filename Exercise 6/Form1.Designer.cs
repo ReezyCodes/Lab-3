7958f17d9938738a3ca876ce86ec299ba94ec422
raulale@student.ubc.ca <raulale@student.ubc.ca>
2021-11-16 20:53:51 -0800

    Add project files.

---
+++ Exercise 6/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Exercise_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(613, 183);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(527, 405);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(454, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(454, 220);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(454, 257);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(454, 294);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 31);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(454, 331);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 31);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(454, 368);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 31);
+            this.textBox6.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(840, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 41);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Connect Serial";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(534, 680);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 41);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Tare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(840, 680);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 41);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ADC Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Voltage";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Average ADC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Weight";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 334);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Standard Deviation";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Stable?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 880);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Exercise 6/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Concurrent;
+using System.IO;
+
+namespace Exercise_6
+{
+	public partial class Form1 : Form
+	{
+		ConcurrentQueue<Int32> dataQueue = new ConcurrentQueue<Int32>();
+		ConcurrentQueue<Int32> averageQueue = new ConcurrentQueue<Int32>();
+		ConcurrentQueue<Int32> stabilityQueue = new ConcurrentQueue<Int32>();
+		string serialDataString = "";
+		int count;
+		int nothing;
+		double tare;
+		double weight;
+
+		int serialPortOpen = 0;
+		int mostImport = 0;
+		int leastImport = 0;
+		int combined;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+
+			int state = 0;
+			int dataByte;
+			double vOut;
+			chart1.ChartAreas[0].AxisY.Maximum = 3.6;
+
+			if (serialPort1.IsOpen)
+			{
+
+				while (dataQueue.TryDequeue(out dataByte))
+				{
+
+					if (state == 0)
+					{
+						if (dataByte == 255)
+						{
+							state = 1;
+						}
+					}
+					else if (state == 1)
+					{
+						mostImport = dataByte;
+
+						state = 2;
+					}
+					else if (state == 2)
+					{
+						leastImport = dataByte;
+						combined = (mostImport << 5) | leastImport;
+						averageQueue.Enqueue(combined);
+						stabilityQueue.Enqueue(combined);
+						textBox1.Text = combined.ToString();
+
+						if (averageQueue.Count >= 100)
+						{
+							double average = averageQueue.Average();
+							vOut = average * 3.6 / 1023;
+							textBox2.Text = vOut.ToString("N3");
+							textBox3.Text = average.ToString("N2");
+							chart1.Series["Series1"].Points.AddXY(count.ToString(), vOut.ToString());
+							count++;
+
+							weight = -995.45 * Math.Round(vOut, 2) + 2462.3 - tare;
+							textBox4.Text = weight.ToString("N2") + " g";
+
+							averageQueue.TryDequeue(out nothing);
+						}
+
+
+
+                        if (stabilityQueue.Count >= 50)
+                        {
+                            int[] stabilityArray = stabilityQueue.ToArray();
+                            double average1 = stabilityArray.Average();
+                            double sumofSquare = stabilityArray.Select(val => (val - average1) * (val - average1)).Sum();
+                            double sd = Math.Sqrt(sumofSquare / stabilityArray.Length);
+                            textBox5.Text = sd.ToString();
+                            if (sd < 3)
+                            {
+                                textBox6.Text = "Yes";
+                            }
+                            else
+                            {
+                                textBox6.Text = "No";
+                            }
+
+                            stabilityQueue.TryDequeue(out nothing);
+                        }
+                        state = 0;
+
+					}
+
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (serialPortOpen == 0)
+			{
+				serialPortOpen = 1;
+				serialPort1.Open();
+				serialPort1.Write("c");
+				button1.Text = "Disconnect Serial";
+			}
+			else if (serialPortOpen == 1)
+			{
+				serialPortOpen = 0;
+				serialPort1.Close();
+
+				button1.Text = "Connect Serial";
+			}
+		}
+
+		private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+			int newbyte = 0;
+			Int32 bytesToRead = serialPort1.BytesToRead;
+			while (bytesToRead > 0)
+			{
+				dataQueue.Enqueue(serialPort1.ReadByte());
+				bytesToRead = serialPort1.BytesToRead;
+				serialDataString = serialDataString + newbyte.ToString() + ", ";
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			tare = weight;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			tare = 0;
+		}
+	}
+}
